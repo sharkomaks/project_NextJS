7eb016d3aa9986939cacc6d8beb9d64115c51aa4
Maksim <sharkomaks@gmail.com>
2024-10-28 18:17:58 +0300

    Структура проекта

--- app/(site)/components/Wrapper/Wrapper.tsx
+++ app/(site)/components/Wrapper/Wrapper.tsx
@@ -1,12 +1,13 @@
+import styles from './Wrapper.module.css';
 import {WrapperProps} from './Wrapper.props';
 
 function Wrapper({children}: WrapperProps) {
 	return (
-		<>
+		<div className={styles['wrapper']}>
 			<header>Header</header>
 			<main>{children}</main>
 			<footer>Footer</footer>
-		</>
+		</div>
 
 	);
 }
