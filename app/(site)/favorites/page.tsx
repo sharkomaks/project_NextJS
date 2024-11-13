f851feb5409044fff0d9fa9c459d90a268d3fe75
Maksim <sharkomaks@gmail.com>
2024-11-13 09:48:40 +0300

    Исправление ошибок

--- app/(site)/favorites/page.tsx
+++ app/(site)/favorites/page.tsx
@@ -1,8 +1,23 @@
-import Favorites from './components/Favorites/Favorites';
+'use client';
 
-export default function Page() {
+import styles from './Favorites.module.css';
+import Card from '@/components/Card/Card';
+import {useContext} from 'react';
+import {UserContext} from '@/context/user.context';
+import Htag from '@/components/Htag/Htag';
+
+function Favorites() {
+
+	const {dataFavorites} = useContext(UserContext);
 
 	return (
-		<Favorites/>
+		<div className={styles['wrapper']}>
+			<Htag tag={'h1'}>Избранное</Htag>
+			<div className={styles['card-list']}>
+				{dataFavorites?.map((f, i) => <Card key={i} product={f}/>)}
+			</div>
+		</div>
 	);
 }
+
+export default Favorites;
